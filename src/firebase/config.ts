562868cf34c9d9c34ac4989f9a7cf327bddb9472
Samwel <samwelabuyeka90@gmail.com>
2025-11-05 09:12:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1912630803-f8faa",
+  "appId": "1:1023345646682:web:5b9433457c1149ca60cc63",
+  "apiKey": "AIzaSyCo5_6ivxe5h7dGLlkr1xkZ0gS3NSKtALw",
+  "authDomain": "studio-1912630803-f8faa.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1023345646682"
+};
